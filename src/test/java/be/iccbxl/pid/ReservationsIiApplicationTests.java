2e9551d3cb10a95a69641f8f26d076dbab9a09ca
feliilunga <ilungasulu@gmail.com>
2021-08-18 14:33:53 +0200

    first commit

---
+++ src/test/java/be/iccbxl/pid/ReservationsIiApplicationTests.java
@@ -0,0 +1,13 @@
+package be.iccbxl.pid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationsIiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
